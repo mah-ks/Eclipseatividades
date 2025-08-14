3e3cdad8ff1a35b1dadbc61da7bb6ded49a2f64d
Maria Karoline <maria_k_da-silva@estudante.sesisenai.org.br>
2025-08-14 11:55:45 -0300

    finalização da atividade

--- Exercicio/src/Cartao/SegundaTela.java
+++ Exercicio/src/Cartao/SegundaTela.java
@@ -1,48 +1,62 @@
 package Cartao;
 
-import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.*;
+import java.awt.*;
 
-public class SegundaTela {
-
-    private JFrame frame;
-
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    SegundaTela window = new SegundaTela();
-                    window.frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    /**
-     * Create the application.
-     */
+public class SegundaTela extends JFrame {
     public SegundaTela() {
-        initialize();
-    }
+        setTitle("Cartão de Visita");
+        setBounds(100, 100, 450, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        getContentPane().setLayout(null);
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() {
-        frame = new JFrame();
-        frame.setTitle("Cartão de Visita");
-        frame.setBounds(100, 100, 450, 300);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
-        frame.setLocationRelativeTo(null); 
+        JTextPane txtpnMariaKarolineDa = new JTextPane();
+        txtpnMariaKarolineDa.setForeground(new Color(168, 129, 91));
+        txtpnMariaKarolineDa.setBackground(new Color(240, 240, 240));
+        txtpnMariaKarolineDa.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 14));
+        txtpnMariaKarolineDa.setEditable(false);
+        txtpnMariaKarolineDa.setText("Maria Karoline Da Silva");
+        txtpnMariaKarolineDa.setBounds(238, 179, 150, 20);
+        getContentPane().add(txtpnMariaKarolineDa);
 
-        JLabel label = new JLabel("<html><center><h2>Maria Silva</h2><br>Desenvolvedora Java<br>maria@email.com</center></html>", SwingConstants.CENTER);
-        frame.getContentPane().add(label);
+        JLabel lblNewLabel = new JLabel("Maria Karoline Da Silva");
+        lblNewLabel.setForeground(new Color(168, 129, 91));
+        lblNewLabel.setFont(new Font("Serif", Font.ITALIC, 18));
+        lblNewLabel.setBounds(34, 44, 232, 24);
+        getContentPane().add(lblNewLabel);
+
+        JLabel lblDesenvolvedora = new JLabel("Desenvolvedora");
+        lblDesenvolvedora.setForeground(new Color(168, 129, 91));
+        lblDesenvolvedora.setFont(new Font("Serif", Font.PLAIN, 16));
+        lblDesenvolvedora.setBounds(34, 79, 232, 24);
+        getContentPane().add(lblDesenvolvedora);
+
+        JSeparator separator = new JSeparator();
+        separator.setForeground(new Color(168, 129, 91));
+        separator.setBounds(10, 134, 414, 2);
+        getContentPane().add(separator);
+
+        JLabel lblNewLabel_2 = new JLabel("Email: Maria.Silva7@br.bosch.com");
+        lblNewLabel_2.setForeground(new Color(168, 129, 91));
+        lblNewLabel_2.setBounds(34, 160, 250, 14);
+        getContentPane().add(lblNewLabel_2);
+
+        JLabel lblNewLabel_5 = new JLabel("M");
+        lblNewLabel_5.setFont(new Font("Serif", Font.PLAIN, 93));
+        lblNewLabel_5.setForeground(new Color(168, 129, 91));
+        lblNewLabel_5.setBounds(249, 134, 84, 102);
+        getContentPane().add(lblNewLabel_5);
+
+        JLabel lblNewLabel_3 = new JLabel("Celular :(47)988548027");
+        lblNewLabel_3.setForeground(new Color(168, 129, 91));
+        lblNewLabel_3.setBounds(34, 198, 232, 14);
+        getContentPane().add(lblNewLabel_3);
+
+        JLabel lblNewLabel_5_1 = new JLabel("K");
+        lblNewLabel_5_1.setForeground(new Color(168, 129, 91));
+        lblNewLabel_5_1.setFont(new Font("Serif", Font.PLAIN, 93));
+        lblNewLabel_5_1.setBounds(327, 134, 84, 102);
+        getContentPane().add(lblNewLabel_5_1);
     }
 }
--- Exercicio/src/Cartao/Visita.java
+++ Exercicio/src/Cartao/Visita.java
@@ -2,6 +2,8 @@ package Cartao;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Image;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
@@ -9,6 +11,8 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.Iterator;
+import java.awt.Font;
+import java.awt.SystemColor;
 
 public class Visita {
 
@@ -36,7 +40,7 @@ public class Visita {
     private void initialize() {
         frmPrincipal = new JFrame();
         frmPrincipal.getContentPane().setBackground(new Color(253, 206, 245));
-        frmPrincipal.setBounds(100, 100, 838, 591);
+        frmPrincipal.setBounds(100, 100, 734, 731);
         frmPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frmPrincipal.getContentPane().setLayout(null);
 
@@ -47,17 +51,22 @@ public class Visita {
         listModel1.addElement("Monitor");
         listModel1.addElement("CPU");
         listModel1.addElement("Notebook");
+        
+        JLabel lblTransferirItens_1 = new JLabel("Itens transferidos");
+        lblTransferirItens_1.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
+        lblTransferirItens_1.setBounds(541, 187, 211, 18);
+        frmPrincipal.getContentPane().add(lblTransferirItens_1);
 
         JList<String> list = new JList<>(listModel1);
-        list.setBackground(new Color(247, 164, 249));
+        list.setBackground(SystemColor.desktop);
         list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        list.setBounds(536, 227, 131, 198);
+        list.setBounds(38, 216, 131, 198);
         frmPrincipal.getContentPane().add(list);
 
         DefaultListModel<String> listModel2 = new DefaultListModel<>();
         JList<String> list_2 = new JList<>(listModel2);
-        list_2.setBackground(new Color(247, 164, 249));
-        list_2.setBounds(677, 227, 131, 198);
+        list_2.setBackground(SystemColor.desktop);
+        list_2.setBounds(560, 216, 131, 198);
         frmPrincipal.getContentPane().add(list_2);
 
         JButton btnConfirmar = new JButton("Confirmar");
@@ -69,24 +78,24 @@ public class Visita {
                 }
             }
         });
-        btnConfirmar.setBounds(614, 436, 120, 23);
+        btnConfirmar.setBounds(38, 425, 120, 23);
         frmPrincipal.getContentPane().add(btnConfirmar);
 
       
         JRadioButton rbtMasculino = new JRadioButton("Masculino");
-        rbtMasculino.setBounds(21, 387, 93, 23);
-        rbtMasculino.setBackground(new Color(247, 164, 249));
+        rbtMasculino.setBounds(378, 556, 93, 23);
+        rbtMasculino.setBackground(SystemColor.desktop);
         buttonGroup_1.add(rbtMasculino);
         frmPrincipal.getContentPane().add(rbtMasculino);
 
         JRadioButton rbtFeminino = new JRadioButton("Feminino");
-        rbtFeminino.setBounds(21, 413, 93, 23);
-        rbtFeminino.setBackground(new Color(247, 164, 249));
+        rbtFeminino.setBounds(578, 556, 93, 23);
+        rbtFeminino.setBackground(SystemColor.desktop);
         buttonGroup_1.add(rbtFeminino);
         frmPrincipal.getContentPane().add(rbtFeminino);
 
         JButton btnSelecionar = new JButton("Inserir");
-        btnSelecionar.setBounds(21, 443, 89, 23);
+        btnSelecionar.setBounds(378, 634, 89, 23);
         btnSelecionar.addActionListener(e -> {
             if (rbtMasculino.isSelected()) {
                 textFM.setText("O genero Masculino foi selecionado");
@@ -112,27 +121,27 @@ public class Visita {
                 new String[]{"Código", "Nome"}
         ));
         JScrollPane scrollPane = new JScrollPane(table);
-        scrollPane.setBounds(536, 25, 272, 94);
+        scrollPane.setBounds(348, 43, 272, 94);
         frmPrincipal.getContentPane().add(scrollPane);
 
 
         JCheckBox ckbJornal = new JCheckBox("Jornal");
-        ckbJornal.setBounds(44, 189, 97, 23);
-        ckbJornal.setBackground(new Color(247, 164, 249));
+        ckbJornal.setBounds(21, 516, 97, 23);
+        ckbJornal.setBackground(SystemColor.desktop);
         frmPrincipal.getContentPane().add(ckbJornal);
 
         JCheckBox cbkRevista = new JCheckBox("Revista");
-        cbkRevista.setBounds(44, 215, 97, 23);
-        cbkRevista.setBackground(new Color(247, 164, 249));
+        cbkRevista.setBounds(21, 542, 97, 23);
+        cbkRevista.setBackground(SystemColor.desktop);
         frmPrincipal.getContentPane().add(cbkRevista);
 
         JCheckBox ckbCdDvd = new JCheckBox("CD/DVD");
-        ckbCdDvd.setBounds(44, 241, 97, 23);
-        ckbCdDvd.setBackground(new Color(247, 164, 249));
+        ckbCdDvd.setBounds(21, 568, 97, 23);
+        ckbCdDvd.setBackground(SystemColor.desktop);
         frmPrincipal.getContentPane().add(ckbCdDvd);
 
         JButton btnSelecionados = new JButton("Itens Selecionados");
-        btnSelecionados.setBounds(163, 215, 189, 23);
+        btnSelecionados.setBounds(21, 599, 189, 23);
         btnSelecionados.addActionListener(e -> {
             String itens = "";
             if (ckbJornal.isSelected()) itens += "\nJornal ";
@@ -144,30 +153,31 @@ public class Visita {
 
  
         JLabel lblMensagem = new JLabel("Digite uma Mensagem");
-        lblMensagem.setBounds(21, 66, 204, 14);
+        lblMensagem.setFont(new Font("Times New Roman", Font.ITALIC, 16));
+        lblMensagem.setBounds(38, 40, 204, 33);
         frmPrincipal.getContentPane().add(lblMensagem);
 
         textField = new JTextField();
-        textField.setBounds(24, 91, 201, 20);
+        textField.setBounds(38, 70, 201, 20);
         textField.setColumns(10);
         frmPrincipal.getContentPane().add(textField);
 
         JButton btnMostrar = new JButton("Mostrar mensagem");
-        btnMostrar.setBounds(21, 122, 189, 23);
+        btnMostrar.setBounds(38, 96, 189, 23);
         btnMostrar.addActionListener(e -> JOptionPane.showMessageDialog(null, textField.getText()));
         frmPrincipal.getContentPane().add(btnMostrar);
 
 
         JSeparator separator = new JSeparator();
-        separator.setBounds(-10, 174, 818, 2);
+        separator.setBounds(-10, 174, 967, 2);
         frmPrincipal.getContentPane().add(separator);
 
         JSeparator separator_1 = new JSeparator();
-        separator_1.setBounds(0, 487, 818, 2);
+        separator_1.setBounds(0, 487, 957, 2);
         frmPrincipal.getContentPane().add(separator_1);
 
         JButton btnSair = new JButton("Sair");
-        btnSair.setBounds(716, 498, 89, 23);
+        btnSair.setBounds(582, 634, 89, 23);
         btnSair.addActionListener(e -> System.exit(0));
         frmPrincipal.getContentPane().add(btnSair);
         
@@ -179,7 +189,7 @@ public class Visita {
             "Verde",
             "Azul"
         }));
-        comboBox.setBounds(536, 130, 265, 22);
+        comboBox.setBounds(348, 141, 265, 22);
         frmPrincipal.getContentPane().add(comboBox);
 
         comboBox.addActionListener(e -> {
@@ -214,9 +224,29 @@ public class Visita {
         
         textFM = new JTextField();
         textFM.setEditable(false);
-        textFM.setBounds(120, 365, 275, 94);
+        textFM.setBounds(378, 586, 292, 37);
         frmPrincipal.getContentPane().add(textFM);
         textFM.setColumns(10);
+        
+        JLabel lblNewLabel = new JLabel("Seleção de Itens");
+        lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
+        lblNewLabel.setBounds(21, 495, 211, 14);
+        frmPrincipal.getContentPane().add(lblNewLabel);
+        
+        JLabel lblTransferirItens = new JLabel("Transferir Itens");
+        lblTransferirItens.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
+        lblTransferirItens.setBounds(38, 187, 211, 18);
+        frmPrincipal.getContentPane().add(lblTransferirItens);
+        
+        JLabel lblNewLabel_1 = new JLabel("New label");
+        lblNewLabel_1.setIcon(new ImageIcon(Visita.class.getResource("/imagem/kitty.png")));
+        lblNewLabel_1.setBounds(0, -35, 1005, 703);
+        frmPrincipal.getContentPane().add(lblNewLabel_1);
+        
+        JLabel lblTransferirItens_2 = new JLabel("Transferir Itens");
+        lblTransferirItens_2.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 16));
+        lblTransferirItens_2.setBounds(541, 187, 211, 18);
+        frmPrincipal.getContentPane().add(lblTransferirItens_2);
         
         JMenuBar menuBar = new JMenuBar();
         frmPrincipal.setJMenuBar(menuBar);
@@ -230,12 +260,28 @@ public class Visita {
         
         JMenuItem mntmNewMenuItem = new JMenuItem("Abrir Mensagem");
         mnNewMenu.add(mntmNewMenuItem);
+        mntmNewMenuItem.addActionListener(e -> {
+            ImageIcon icon = new ImageIcon("kitty.png");    
+            Image img = icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            icon = new ImageIcon(img);
+
+            JOptionPane.showMessageDialog(
+                frmPrincipal,
+                "Olá tudo bem?",
+                "Mensagem",
+                JOptionPane.INFORMATION_MESSAGE,
+                icon
+            );
+        });
+
+
         
         JMenuItem mntmNewMenuItem_1 = new JMenuItem("Abrir Cartão de Visita");
         mnNewMenu.add(mntmNewMenuItem_1);
         mntmNewMenuItem_1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                new SegundaTela(); 
+            	SegundaTela st = new SegundaTela();
+				st.setVisible(true);
             }
         });
     
